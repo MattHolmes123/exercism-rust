0ebedec07b0836dff5ca8724628cc43820dfcf61
Matthew Holmes <matt_holmes123@hotmail.com>
2020-04-09 08:46:36 +0100

    Refactor proverb solution.

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,24 +1,30 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    // then all n, n + 1 pairs (until n-1)
-    // then first element (for the end)
-
-    // # https://doc.rust-lang.org/std/primitive.slice.html#method.chunks_exact
-
     if list.is_empty() {
         return String::new();
     }
 
-    let mut proverbs: Vec<String> = vec![];
+    // Commented out original solultion
+    // let mut proverbs: Vec<String> = vec![];
 
-    for (i, item) in list[0..list.len() - 1].iter().enumerate() {
-        proverbs.push(format!(
-            "For want of a {} the {} was lost.",
-            item,
-            list[i + 1]
-        ));
-    }
+    // for (i, item) in list[0..list.len() - 1].iter().enumerate() {
+    //     proverbs.push(format!(
+    //         "For want of a {} the {} was lost.",
+    //         item,
+    //         list[i + 1]
+    //     ));
+    // }
 
-    proverbs.push(format!("And all for the want of a {}.", list[0]));
+    // proverbs.push(format!("And all for the want of a {}.", list[0]));
 
-    proverbs.join("\n")
+    // proverbs.join("\n")
+
+    // https://doc.rust-lang.org/std/primitive.slice.html#method.windows
+    list.windows(2)
+        .map(|slice| format!("For want of a {} the {} was lost.", slice[0], slice[1]))
+        .chain(std::iter::once(format!(
+            "And all for the want of a {}.",
+            list[0]
+        )))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
