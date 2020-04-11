675f2388945984307bfc3a795bc9b2b6fe06c536
Matthew Holmes <matt_holmes123@hotmail.com>
2020-04-11 11:47:40 +0100

    Complete grains exercise.

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,13 @@
+pub const MAX: u64 = u64::max_value();
+
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64")
+    }
+
+    MAX & (1 << (s - 1))
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..=64).map(square).sum()
 }
