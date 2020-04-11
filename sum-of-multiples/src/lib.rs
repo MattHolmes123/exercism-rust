b27792cb7b90f4d73d1969baf84fe7c2b17786eb
Matthew Holmes <matt_holmes123@hotmail.com>
2020-04-11 11:06:23 +0100

    Add initial version of sum-of-multiples.

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,18 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    let mut common_multiples: HashSet<u32> = HashSet::new();
+    common_multiples.insert(0);
+
+    for &m in factors {
+        if m == 0 {
+            continue;
+        }
+
+        for x in (m..limit).step_by(m as usize) {
+            common_multiples.insert(x);
+        }
+    }
+
+    common_multiples.iter().sum::<u32>()
 }
