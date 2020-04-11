d0c584f0166ea8d173e051c25e844232d674d52f
Matthew Holmes <matt_holmes123@hotmail.com>
2020-04-11 12:17:28 +0100

    Add initial version of prime-factors (Copied from my old python version).

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut primes = vec![];
+
+    let mut value = n;
+
+    for i in 2..=n {
+        while value % i == 0 {
+            primes.push(i);
+            value /= i;
+        }
+
+        if value == 1 {
+            break;
+        }
+    }
+
+    primes
 }
