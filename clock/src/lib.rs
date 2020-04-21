217773cc438df99b3eb92941a55001ca09d1fbae
Matthew Holmes <matt_holmes123@hotmail.com>
2020-04-21 07:51:40 +0100

    Initial Solution to Clock exercise.

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,34 @@
-pub struct Clock;
+use std::fmt;
+use chrono::{NaiveTime, Duration, Timelike};
+
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Clock {
+    nt: NaiveTime
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let mut nt = NaiveTime::from_hms(0, 0, 0);
+
+        nt += Duration::hours(hours as i64);
+        nt += Duration:: minutes(minutes as i64);
+
+        Clock {nt: nt}
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        let mut nt = NaiveTime::from_hms(
+            self.nt.hour(), self.nt.minute(), self.nt.second()
+        );
+        nt += Duration::minutes(minutes as i64);
+
+        Clock {nt: nt}
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.nt.hour(), self.nt.minute())
     }
 }
