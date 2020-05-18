2098fde6f72d81a4ab99913af8587162de52f547
Matthew Holmes <matt_holmes123@hotmail.com>
2020-05-18 07:26:58 +0100

    Update clock solution to use Euclidean division.

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,34 +1,36 @@
 use std::fmt;
-use chrono::{NaiveTime, Duration, Timelike};
 
+const MINS_PER_DAY: i32 = 24 * 60;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Clock {
-    nt: NaiveTime
+    mins: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut nt = NaiveTime::from_hms(0, 0, 0);
+        let mins = as_clock_time((hours * 60) + minutes);
 
-        nt += Duration::hours(hours as i64);
-        nt += Duration:: minutes(minutes as i64);
-
-        Clock {nt: nt}
+        Clock { mins }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let mut nt = NaiveTime::from_hms(
-            self.nt.hour(), self.nt.minute(), self.nt.second()
-        );
-        nt += Duration::minutes(minutes as i64);
+        let mins = as_clock_time(self.mins + minutes);
 
-        Clock {nt: nt}
+        Clock { mins }
     }
+}
+
+/// Calculates the least nonnegative remainder of mins
+pub fn as_clock_time(minutes: i32) -> i32 {
+    minutes.rem_euclid(MINS_PER_DAY)
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.nt.hour(), self.nt.minute())
+        let hours = self.mins / 60;
+        let minutes = self.mins % 60;
+
+        write!(f, "{:02}:{:02}", hours, minutes)
     }
 }
